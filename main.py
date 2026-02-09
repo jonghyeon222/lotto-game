dedeb3938bcd2aaf2782b69b4c0d4913192ae667
jjong <shin530710@gmail.com>
2026-02-09 15:30:06 +0900

    로또 번호만 생성하는 단계

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import random
-import tkinter
-import tkinter.font
 
 lotto_num = range(1,46)
 
-def buttonClick():
+for i in range(5):
     print(random.sample(lotto_num,6))
-
-
-window=tkinter.Tk()
-window.title("lotto")
-window.geometry("400x200")
-window.resizable(False, False)
-
-button = tkinter.Button(window, overrelief="solid",
-text="번호확인", width=15, command=buttonClick, repeatdelay=1000, repeatinterval=100)
-button.pack()
-
-window.mainloop()
